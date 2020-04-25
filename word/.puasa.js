ff88ace3bde89df23e764d94a83d313fcf3999df
tegarsatya <59832647+tegarsatya@users.noreply.github.com>
2020-04-24 14:48:30 -1200

    first commit

---
+++ word/.puasa.js
@@ -0,0 +1,11 @@
+// <!-- #Dirumah_Aja, #Jangan_mudik -->
+System.out.printIn("Selamat Berbuka Puasa")
+if (Bagi_Yang_Menjalankan_Ibadah_Puasa) {
+    System.out.printIn("jangan lupa berbuka")
+} else {
+    System.out.PrintIn("Dengan yang manis-manis dan bergizi", 
+    "jangan lupa tetap dirumah aja dan jaga jarak, Physical Distancing
+    ")
+}  
+
+// <!--Tegar Satya Negara -->
---
+++ word/.puasa.php
@@ -0,0 +1,12 @@
+<!-- #Dirumah_Aja, #Jangan_mudik -->
+<?php
+System.out.println("Selamat Berbuka Puasa");
+if ($Bagi_Yang_Menjalankan_Ibadah_Puasa = $input) {
+    System.out.println("jangan lupa berbuka");
+}else {
+    System.out.Println("Dengan yang manis-manis dan bergizi,
+    tetap dirumah aja dan jaga jarak, Physical Distancing, I Love You Ria Silfi ");
+}
+
+?>
+<!-- Tegar Satya Negara -->
